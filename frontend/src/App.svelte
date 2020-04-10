dda7dd6d9a9954ab880dcc30b1f69d16a5a30c99
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2020-04-10 09:21:25 -0300

    Replace frontend lib from React by Svelte

---
+++ frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import About from "./routes/About.svelte";
+
+  export let url = "";
+  export let name = "";
+</script>
+
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+  </nav>
+  <div>
+    <Route path="/"><Home name={name} /></Route>
+    <Route path="/about"><About /></Route>
+  </div>
+</Router>
